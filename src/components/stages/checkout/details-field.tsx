8836b29a72f573f9617a6efb12e0af372b3d6176
David Harush <davidharush@gmail.com>
2019-05-01 22:48:48 +0300

    get email from store

--- src/components/stages/checkout/details-field.tsx
+++ src/components/stages/checkout/details-field.tsx
@@ -4,11 +4,13 @@ import "./details-field.css";
 export const DetailsField = ({
   fieldName,
   onChange,
+  defaultValue,
   isValid = true
 }: {
   fieldName: string;
   onChange: (value: string) => void;
   isValid?: boolean;
+  defaultValue?: string;
 }) => {
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     onChange && onChange(event.target.value);
@@ -16,14 +18,18 @@ export const DetailsField = ({
     "order-details-field" + (!isValid ? " error-color" : "");
   return (
     <div className={fieldClassName}>
-      <span>{`${fieldName}:`}</span>
+      <span className="field-name">{`${fieldName}:`}</span>
       <div>
         {
           <div className="error-message error-color">
             {!isValid ? "invalid value" : ""}
           </div>
         }
-        <input type="text" onChange={handleOnChange} />
+        <input
+          type="text"
+          onChange={handleOnChange}
+          defaultValue={defaultValue}
+        />
       </div>
     </div>
   );
