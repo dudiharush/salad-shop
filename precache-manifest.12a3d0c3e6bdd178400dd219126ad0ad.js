e7099e3a676ef7ecbce0a42c70db82c5843bcb64
David Harush <davidharush@gmail.com>
2019-05-01 10:43:45 +0300

    Updates

---
+++ precache-manifest.12a3d0c3e6bdd178400dd219126ad0ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acb682eddf1c8375b8fc36479121074",
+    "url": "/salad-shop-1/index.html"
+  },
+  {
+    "revision": "f796b5160af959a938ce",
+    "url": "/salad-shop-1/static/css/main.d57ad54f.chunk.css"
+  },
+  {
+    "revision": "971fcad114095eea6838",
+    "url": "/salad-shop-1/static/js/2.a7397fb3.chunk.js"
+  },
+  {
+    "revision": "f796b5160af959a938ce",
+    "url": "/salad-shop-1/static/js/main.19c812cc.chunk.js"
+  },
+  {
+    "revision": "e94577f864a50d7f9f4f",
+    "url": "/salad-shop-1/static/js/runtime~main.cd333344.js"
+  }
+]);
